bb34de37d309d198b6ff4d6c3a8cc737aa018f93
Sripoorimadevi <Sripoornimadevi24@gmail.com>
2025-03-27 14:46:27 +0530

    Initial Commit from eclipse

---
+++ src/main/java/com/Elecxa1Application.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Elecxa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Elecxa1Application.class, args);
+	}
+
+}
