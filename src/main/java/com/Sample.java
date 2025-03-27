cb89719ad010e52bb96c7e2d9fb86e73186bc362
Selvaganapathi-414 <selvaganapathi414@gmail.com>
2025-03-27 16:12:45 +0530

    Second Commit

--- src/main/java/com/Sample.java
+++ src/main/java/com/Sample.java
@@ -5,5 +5,7 @@ public class Sample {
 	public static void main(String args[]) {
 		 System.out.println("Hello world");
 		 System.out.println("This is Sample");
+		 System.out.println("This is Sample2");
+
 	}
 }
